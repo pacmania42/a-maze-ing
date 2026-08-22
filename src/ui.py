bbcfea4ac6144f846f6f4876bd4978b9db7f460b
Semere M. Mebrahtom <88221834+semere-meb@users.noreply.github.com>
2026-08-22 15:59:26 +0200

    26: mlx 04 render shortest path overlay (#65)
    * painting path cells
    
    * painting path; moved render method to UI class

--- src/ui.py
+++ src/ui.py
@@ -23,7 +23,6 @@ class UI:
         self.adapter = adapter
         rows, columns = len(self.adapter.grid), len(self.adapter.grid[0])
 
-        self.show_terminals = False
         self.m = Mlx()
         self.mlx_ptr: int = self.m.mlx_init()
         self.width = columns * stg.cell_size
@@ -49,42 +48,107 @@ class UI:
         if keycode == 65307:  # escape
             self.m.mlx_destroy_window(self.mlx_ptr, self.win_ptr)
             os._exit(0)
-        if keycode == 116:  # t
-            self.show_terminals = not self.show_terminals
-            self.paint_terminals()
 
     def show(self) -> None:
-        for row in self.adapter.grid:
-            for cell in row:
-                cell.render(self.data_addr, self.ll, self.bpp)
+        self.render_maze()
+        self.render_terminals()
+        self.render_path()
 
         self.m.mlx_put_image_to_window(
             self.mlx_ptr, self.win_ptr, self.img_addr, 0, 0
         )
         self.m.mlx_loop(self.mlx_ptr)
 
-    def paint_terminals(self) -> None:
+    def render_terminals(self) -> None:
         terminals = (self.adapter.entry, "entry"), (self.adapter.exit, "exit")
 
         for cell, name in terminals:
             x = cell.row * stg.cell_size
             y = cell.col * stg.cell_size
 
-            if not self.show_terminals:
-                color = stg.off_color
-            else:
-                color = stg.entry_color if name == "entry" else stg.exit_color
+            color = stg.entry_color if name == "entry" else stg.exit_color
 
-            Cell.put_box(
-                data_addr=self.data_addr,
-                line_len=self.ll,
-                bpp=self.bpp,
+            self._put_box(
                 y=x + stg.wall_size,
                 x=y + stg.wall_size,
                 width=int(stg.cell_size - 2 * stg.wall_size),
                 height=int(stg.cell_size - 2 * stg.wall_size),
                 color=color,
             )
-        self.m.mlx_put_image_to_window(
-            self.mlx_ptr, self.win_ptr, self.img_addr, 0, 0
-        )
+
+    def render_path(self) -> None:
+        path = self.adapter.shortest_path[1:-1]
+
+        for cell in path:
+            x = cell.row * stg.cell_size
+            y = cell.col * stg.cell_size
+
+            self._put_box(
+                y=x + stg.wall_size,
+                x=y + stg.wall_size,
+                width=int(stg.cell_size - 2 * stg.wall_size),
+                height=int(stg.cell_size - 2 * stg.wall_size),
+                color=stg.path_color,
+            )
+
+    def _put_pixel(self, y: int, x: int, color: int) -> None:
+        offset = (y * self.ll) + (x * (self.bpp // 8))
+        self.data_addr[offset] = (color) & 0xFF
+        self.data_addr[offset + 1] = (color >> 8) & 0xFF
+        self.data_addr[offset + 2] = (color >> 16) & 0xFF
+        self.data_addr[offset + 3] = 0xFF
+
+    def _put_box(
+        self, y: int, x: int, width: int, height: int, color: int
+    ) -> None:
+        for yy in range(height):
+            for xx in range(width):
+                self._put_pixel(y=y + yy, x=x + xx, color=color)
+
+    def render_maze(
+        self,
+    ) -> None:
+        for row in self.adapter.grid:
+            for cell in row:
+                y = cell.row * stg.cell_size
+                x = cell.col * stg.cell_size
+
+                # north
+                if cell.n:
+                    self._put_box(
+                        y=y,
+                        x=x,
+                        width=stg.cell_size,
+                        height=stg.wall_size,
+                        color=stg.wall_color,
+                    )
+
+                # south
+                if cell.s:
+                    self._put_box(
+                        y=y + stg.cell_size - stg.wall_size,
+                        x=x,
+                        width=stg.cell_size,
+                        height=stg.wall_size,
+                        color=stg.wall_color,
+                    )
+
+                # east wall
+                if cell.e:
+                    self._put_box(
+                        y=y,
+                        x=x + stg.cell_size - stg.wall_size,
+                        width=stg.wall_size,
+                        height=stg.cell_size,
+                        color=stg.wall_color,
+                    )
+
+                # west
+                if cell.w:
+                    self._put_box(
+                        x=x,
+                        y=y,
+                        width=stg.wall_size,
+                        height=stg.cell_size,
+                        color=stg.wall_color,
+                    )
