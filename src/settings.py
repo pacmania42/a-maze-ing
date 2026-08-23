a4ec0fafe53a071975d4698bb313d69987e34837
Semere M. Mebrahtom <88221834+semere-meb@users.noreply.github.com>
2026-08-23 22:10:09 +0200

    29: mlx 07 interaction change wall colours (#71)

--- src/settings.py
+++ src/settings.py
@@ -1,17 +1,24 @@
 class Settings:
-    cell_size = 50
-    wall_size = cell_size // 10
+    def __init__(self) -> None:
+        self.cell_size = 50
+        self.wall_size = self.cell_size // 10
 
-    footer_height = 160
-    text_x_offset = 30
-    text_y_offset = 30
-    text_line_inset = 25
+        self.footer_height = 160
+        self.text_x_offset = 30
+        self.text_y_offset = 30
+        self.text_line_inset = 25
 
-    off_color = 0x000000  # black
-    wall_color = 0x0000FF  # blue
-    path_color = 0x00FF00  # green
-    entry_color = 0xEAF6AD  # light shade of green
-    exit_color = 0xB6DB00  # dark shade of green
-    text_color = 0xFFFFFF  # white
+        self.wall_colors = [
+            0x0000FF,
+            0xDDDDDD,
+            0xFFFFFF,
+            0x00FFFF,
+        ]
 
-    window_title: str = "a-maze-ing"
+        self.off_color = 0x000000  # black
+        self.path_color = 0x00FF00  # green
+        self.entry_color = 0xEAF6AD  # light shade of green
+        self.exit_color = 0xB6DB00  # dark shade of green
+        self.text_color = 0xFFFFFF  # white
+
+        self.window_title: str = "a-maze-ing"
